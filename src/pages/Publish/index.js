8f976e60f537dbaa2698def8f558cac898b755a4
Calista <2320035651@qq.com>
2024-01-10 20:49:38 +0800

    根据id适配编辑和新增状态

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -81,7 +81,11 @@ const Publish = () => {
         })
       );
     }
-    getArticleDetail();
+    //只有有id的时候才能调用此函数
+    if (articleId) {
+      getArticleDetail();
+    }
+
     // 2，调用实例方法 完成回填
   }, [articleId, form]);
   return (
@@ -91,7 +95,7 @@ const Publish = () => {
           <Breadcrumb
             items={[
               { title: <Link to={"/"}>首页</Link> },
-              { title: "发布文章" },
+              { title: `${articleId ? "编辑" : "发布"}文章` },
             ]}
           />
         }
